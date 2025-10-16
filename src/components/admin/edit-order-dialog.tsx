863ac4047669f96118a0349a5a4f974f1a67736e
lonshanworld <lonshan3010@gmail.com>
2025-10-16 16:32:05 +0700

    3 add oh-hold

--- src/components/admin/edit-order-dialog.tsx
+++ src/components/admin/edit-order-dialog.tsx
@@ -41,7 +41,7 @@ const transactionFormSchema = z.object({
   merchantOrderId: z.string().min(1, "Merchant Transaction ID is required."),
   customerName: z.string().min(2, "Customer name is required."),
   customerEmail: z.string().email("Please enter a valid email."),
-  status: z.enum(["Pending", "Completed", "Failed", "Requires Confirmation", "Refunded", "Reconciled", "Partially Paid"]),
+  status: z.enum(["Pending", "Completed", "Failed", "Requires Confirmation", "Refunded", "Reconciled", "Partially Paid", "On-Hold"]),
   orderAmount: z.coerce.number().positive("Amount must be a positive number."),
   totalAmount: z.coerce.number().positive("Amount must be a positive number."),
   paidAmount: z.coerce.number().min(0, "Amount must be zero or positive."),
